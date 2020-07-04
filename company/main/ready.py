2d4f06d6cbbbd4380e53436668f2fd31816aa4d7
mhybodhi <2436810573@qq.com>
2020-07-04 20:12:05 +0800

    星期六

--- company/main/ready.py
+++ company/main/ready.py
@@ -28,7 +28,7 @@ def main(baudrate,args,server):
     sers = []
     for device in devices:
         try:
-            sers.append(serial.Serial(device, baudrate, timeout=None,rtscts=True))
+            sers.append(serial.Serial(device, baudrate, timeout=None))
         except:
             pass
     plist = []
