00eb9b56465f0ee00f85c36ad45a70a429e017d8
mhybodhi <2436810573@qq.com>
2020-07-05 20:15:16 +0800

    ok...

--- company/install.sh
+++ company/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 test -e ~/.pip/pip.conf
+cat > /etc/apt/sources.list <<-MHY
+    deb http://zhangchao-2:igentwva@archive.kylinos.cn/kylin/KYLIN-ALL 4.0.2sp2 main restricted universe multiverse
+    deb http://archive.kylinos.cn/kylin/KYLIN-ALL 4.0.2sp2-desktop main restricted universe multiverse
+MHY
 if [ $? -ne 0 ];then
 	mkdir ~/.pip
 	cat > ~/.pip/pip.conf <<-MHY
@@ -9,4 +13,6 @@ if [ $? -ne 0 ];then
 		trusted-host = pypi.tuna.tsinghua.edu.cn	
 	MHY
 fi
-sudo apt -y update && sudo apt -y install python3-pip && sudo python3 -m pip install --upgrade pip && sudo python3 -m pip install pyserial && pip install redis && sudo python3 -m pip install requests
+sudo apt -y update && sudo apt -y install python3-pip
+sudo python3 -m pip install --upgrade pip && sudo python3 -m pip install pyserial
+sudo python3 -m pip install redis && sudo python3 -m pip install requests
