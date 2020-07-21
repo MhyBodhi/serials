d1e65d1aac3fd9cd8bb7992cf9b16c6c64ef2db1
mhybodhi <2436810573@qq.com>
2020-07-21 23:26:48 +0800

    ok...

--- company/realize/trserial.py
+++ company/realize/trserial.py
@@ -233,7 +233,7 @@ class TRSerial():
                 elif len(self.startcontent) == 256:
                     self.ac_success += 1
                     # 统计接收数据速率
-                    self.receive_speed += 256 / (self.receive_end - self.receive_start) / 1024
+                    self.receive_speed += self.bytes_number / (self.receive_end - self.receive_start) / 1024
                     print("receive_speed",self.receive_speed)
                 logging.info("测试通过!")
         self.writecsv()
