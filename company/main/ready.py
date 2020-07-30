57c9bb0fb74b584076a7ebd2cc825c0fd5b22d7c
mhybodhi <2436810573@qq.com>
2020-07-30 11:53:18 +0800

    ok

--- company/main/ready.py
+++ company/main/ready.py
@@ -1,7 +1,7 @@
 import csv
 import os
 import time
-from multiprocessing import Lock
+from multiprocessing import Process,Lock
 from threading import Thread
 import serial
 from realize.tserial import TSerial
@@ -36,7 +36,7 @@ def main(baudrate,args,server):
         print("设备读取失败")
         return
     for ser in sers:
-        plist.append(Thread(target=run, args=(ser,server,args)))
+        plist.append(Process(target=run, args=(ser,server,args)))
     for i in plist:
         i.start()
     for j in plist:
